06a03b7cef73a9476566af8a6fde5dbd7712783d
AndreyChacon <58754471+AndreyChacon@users.noreply.github.com>
2020-12-04 22:01:24 -0500

    Subo primer ejercicio

---
+++ quiz.js
@@ -0,0 +1,14 @@
+
+
+var TipoMotor  = 4;
+switch (TipoMotor) {
+    case 1: document.write('La bomba es una bomba de agua' + '<br/>' + '<br/>'); break;
+    case 2: document.write('La bomba es una bomba de gasolina' + '<br/>' + '<br/>'); break;
+    case 3: document.write('La bomba es una bomba de hormigón' + '<br/>' + '<br/>'); break;
+    case 4: document.write('La bomba es una bomba de pasta alimenticia' + '<br/>' + '<br/>'); break;
+    default: document.write('No existe un valor válido para tipo de bomba' + '<br/>' + '<br/>');
+}
+
+
+
+
